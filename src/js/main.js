b191c8ed3e0da8a2adf2d1ef0c9fe2ebb16100bc
EduardoAlparov <workman133@yandex.ru>
2023-07-02 01:30:34 +0300

    init

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+import './lazyload';
+import detectTouch from './detectTouch';
+import setScrollbarWidth from './setScrollbarWidth';
+import masks from './masks';
+import validation from './validation';
+import anchorLinks from './anchorLinks';
+import accordions from './accordions';
+import modals from './modals';
+import tabs from './tabs';
+import menu from './menu';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', function () {
+    detectTouch();
+    setScrollbarWidth();
+    masks();
+    validation();
+    anchorLinks();
+    accordions();
+    modals();
+    tabs();
+    menu();
+});
+
+document.addEventListener('lazyloaded', () => {
+    ScrollTrigger.refresh();
+});
+
+window.addEventListener('load', function () {
+    document.body.classList.add('loaded');
+    ScrollTrigger.refresh();
+    setTimeout(() => document.body.classList.add('animatable'), 300);
+});
